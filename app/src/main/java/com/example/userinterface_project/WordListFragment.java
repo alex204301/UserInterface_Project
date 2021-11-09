9eb9081ee4d8c6a7868dc00aac33531b910c21c5
hwk06 <hwk06799@naver.com>
2021-11-09 21:13:27 +0900

    단어 목록 -> 단어장 메뉴 전환

--- app/src/main/java/com/example/userinterface_project/WordListFragment.java
+++ app/src/main/java/com/example/userinterface_project/WordListFragment.java
@@ -1,5 +1,6 @@
 package com.example.userinterface_project;
 
+import android.content.ComponentName;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
@@ -31,10 +32,12 @@ public class WordListFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_word_list, container,false);
+
         ActionBar actionBar = ((MainActivity)getActivity()).getSupportActionBar();
-        actionBar.setTitle("단어 목록 - 단어장_이름1");
+        actionBar.setTitle("단어장_이름1");
         actionBar.setDisplayHomeAsUpEnabled(true);
         setHasOptionsMenu(true);
+
         FloatingActionButton fab = rootView.findViewById(R.id.tab1_plus_btn);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -47,10 +50,19 @@ public class WordListFragment extends Fragment {
     }
 
     @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.menu1, menu);
+    }
+
+    @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         if (item.getItemId() == android.R.id.home) {
             ((MainActivity)getActivity()).replaceFragment(Tab1Fragment.newInstance());
             return true;
+        }else if(item.getItemId() == R.id.menu1) {
+            Intent intent = new Intent((MainActivity)getActivity(), FilterActivity.class);
+            startActivity(intent);
         }
         return super.onOptionsItemSelected(item);
     }
