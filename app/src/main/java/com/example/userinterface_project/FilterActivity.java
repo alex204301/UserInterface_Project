5ae2447968ff2fce8904567a4a657bed643bc769
hwk06 <hwk06799@naver.com>
2021-11-16 00:04:47 +0900

    FilterActivity 일부 작성(정렬/필터 기능 미구현)

--- app/src/main/java/com/example/userinterface_project/FilterActivity.java
+++ app/src/main/java/com/example/userinterface_project/FilterActivity.java
@@ -7,8 +7,21 @@ import android.content.ComponentName;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.RadioGroup;
 
-public class FilterActivity extends AppCompatActivity {
+public class FilterActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener, View.OnClickListener {
+
+    private RadioGroup radioGroup;
+    private CheckBox checkBoxEasy;
+    private CheckBox checkBoxNormal;
+    private CheckBox checkBoxHard;
+    private CheckBox checkBoxWord;
+    private CheckBox checkBoxMeaning;
+    private Button buttonApply;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +32,25 @@ public class FilterActivity extends AppCompatActivity {
         assert actionBar != null;
         actionBar.setDisplayHomeAsUpEnabled(true);
         actionBar.setTitle("단어장 메뉴");
+
+        radioGroup = findViewById(R.id.radio_group_difficulty);
+        checkBoxEasy = findViewById(R.id.checkbox_easy);
+        checkBoxNormal = findViewById(R.id.checkbox_normal);
+        checkBoxHard = findViewById(R.id.checkbox_hard);
+        checkBoxWord = findViewById(R.id.checkbox_word);
+        checkBoxMeaning = findViewById(R.id.checkbox_meaning);
+        buttonApply = findViewById(R.id.button_apply);
+
+        if (savedInstanceState == null) {
+            radioGroup.check(R.id.radio_recent);
+        } // 첫 실행 시 기본값으로 최근 등록 순 선택
+
+        checkBoxEasy.setOnCheckedChangeListener(this);
+        checkBoxNormal.setOnCheckedChangeListener(this);
+        checkBoxHard.setOnCheckedChangeListener(this);
+        checkBoxWord.setOnCheckedChangeListener(this);
+        checkBoxMeaning.setOnCheckedChangeListener(this);
+        buttonApply.setOnClickListener(this);
     }
 
     @Override
@@ -28,5 +60,19 @@ public class FilterActivity extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        //난이도가 모두 unchecked 되거나, 단어와 뜻이 모두 unchecked 되는 것을 막는다.
+        if(!(checkBoxEasy.isChecked()||checkBoxNormal.isChecked()||checkBoxHard.isChecked()))
+            buttonView.setChecked(true);
+        if(!(checkBoxWord.isChecked()||checkBoxMeaning.isChecked()))
+            buttonView.setChecked(true);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
